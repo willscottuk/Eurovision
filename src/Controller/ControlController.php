ffbeb4642fff98498a1b97c93d5b3c9dab63c0b8
Will Scott <willscottuk@users.noreply.github.com>
2018-05-06 13:26:31 +0100

    Initial GodMode

---
+++ src/Controller/ControlController.php
@@ -0,0 +1,67 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+use Cake\ORM\TableRegistry;
+use Cake\Datasource\ConnectionManager;
+
+
+/**
+ * Screen Controller
+ *
+ * @property \App\Model\Table\ScreenTable $Screen
+ */
+class ScreenController extends AppController
+{
+
+    /**
+     * Index method
+     *
+     * @return \Cake\Network\Response|null
+     */
+    public function index()
+    {
+
+        # Return current state
+
+        $this->set(compact('dash'));
+        $this->set('_serialize', ['dash']);
+    }
+
+	public function initialize()
+	{
+	  parent::initialize();
+		$this->Auth->allow(['index', 'view']);
+	}
+    /**
+     * God mode method
+     *
+     * @param string|null $id Screen id.
+     * @return \Cake\Network\Response|null
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function godmode()
+    {
+
+      # Get current mode
+
+      $conn = ConnectionManager::get('default');
+  		$stmt = $conn->execute("SELECT * from control ORDER BY id DESC LIMIT 1");
+  		$mode = $stmt->fetchAll('assoc');
+
+        $this->set(compact('mode'));
+        $this->set('_serialize', ['mode']);
+    }
+
+    public function isAuthorized($user)
+	{
+	    $action = $this->request->getParam('action');
+
+	    // The add and index actions are always allowed.
+	    if (in_array($action, ['index', 'view', 'tags'])) {
+	        return true;
+	    }
+	}
+
+
+    }
